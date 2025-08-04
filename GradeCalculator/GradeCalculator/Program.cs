ea3f65b7a7600a582d901f72a7c1f11b6a5580b0
Mensah Gad Kwame <speedsterblack92@gmail.com>
2025-08-04 13:15:38 +0000

    edited

--- GradeCalculator/GradeCalculator/Program.cs
+++ GradeCalculator/GradeCalculator/Program.cs
@@ -1,51 +1,60 @@
-﻿int grade;
-
-while (true)
+﻿namespace GradeCalculator
 {
-    //Error handling using try..catch block
-    try
+    internal class GradeCalculator
     {
-        Console.WriteLine("Please enter your grade between 0 and 100");
-        string input = Console.ReadLine();
+        static void Main(string[] args)
+        {
+            int grade;
 
-        // Convert user input to integer
-        grade = Convert.ToInt32(input);
+            while (true)
+            {
+                //Error handling using try..catch block
+                try
+                {
+                    Console.WriteLine("Please enter a numerical grade between 0 and 100");
+                    string input = Console.ReadLine();
 
-        //Check grade range
-        if (grade < 0 || grade > 100)
-        {
-            Console.WriteLine("Please enter a number between 0 and 100....");
-            continue;
-        }
+                    // Convert user input to integer
+                    grade = Convert.ToInt32(input);
 
-        if (grade >= 90)
-        {
-            Console.WriteLine("Your grade is A");
-        }
-        else if (grade >= 80)
-        {
-            Console.WriteLine("Your grade is B");
-        }
-        else if (grade >= 70)
-        {
-            Console.WriteLine("Your grade is  C");
-        }
-        else if (grade >= 60)
-        {
-            Console.WriteLine("Your grade is D");
-        }
-        else
-        {
-            Console.WriteLine("Your grade is F");
-        }
+                    //Check grade range
+                    if (grade < 0 || grade > 100)
+                    {
+                        Console.WriteLine("Please enter a number between 0 and 100....");
+                        continue;
+                    }
 
-        break; // Break loop
+                    if (grade >= 90)
+                    {
+                        Console.WriteLine("Your grade is A");
+                    }
+                    else if (grade >= 80)
+                    {
+                        Console.WriteLine("Your grade is B");
+                    }
+                    else if (grade >= 70)
+                    {
+                        Console.WriteLine("Your grade is  C");
+                    }
+                    else if (grade >= 60)
+                    {
+                        Console.WriteLine("Your grade is D");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Your grade is F");
+                    }
 
-    }
-    catch (FormatException fe)
-    {
-        Console.WriteLine("Please enter a number.....");
-        Console.WriteLine(fe.Message);
-        continue; // Continue loop
+                    break; // Break loop
+
+                }
+                catch (FormatException fe)
+                {
+                    Console.WriteLine("Please enter a number.....");
+                    Console.WriteLine(fe.Message);
+                    continue; // Continue loop
+                }
+            }
+        }
     }
 }
