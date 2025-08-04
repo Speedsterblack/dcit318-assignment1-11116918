0f13c7a5daf798205bf739bbf08467a4bd6c399a
Mensah Gad Kwame <speedsterblack92@gmail.com>
2025-08-04 13:06:45 +0000

    Grade calculator completed

---
+++ GradeCalculator/GradeCalculator/Program.cs
@@ -0,0 +1,51 @@
+﻿int grade;
+
+while (true)
+{
+    //Error handling using try..catch block
+    try
+    {
+        Console.WriteLine("Please enter your grade between 0 and 100");
+        string input = Console.ReadLine();
+
+        // Convert user input to integer
+        grade = Convert.ToInt32(input);
+
+        //Check grade range
+        if (grade < 0 || grade > 100)
+        {
+            Console.WriteLine("Please enter a number between 0 and 100....");
+            continue;
+        }
+
+        if (grade >= 90)
+        {
+            Console.WriteLine("Your grade is A");
+        }
+        else if (grade >= 80)
+        {
+            Console.WriteLine("Your grade is B");
+        }
+        else if (grade >= 70)
+        {
+            Console.WriteLine("Your grade is  C");
+        }
+        else if (grade >= 60)
+        {
+            Console.WriteLine("Your grade is D");
+        }
+        else
+        {
+            Console.WriteLine("Your grade is F");
+        }
+
+        break; // Break loop
+
+    }
+    catch (FormatException fe)
+    {
+        Console.WriteLine("Please enter a number.....");
+        Console.WriteLine(fe.Message);
+        continue; // Continue loop
+    }
+}
